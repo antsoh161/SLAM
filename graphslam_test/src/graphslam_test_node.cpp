c6749a73dda89ab6c2f24c779d4567413cd9550e
Anton S <antso@kth.se>
2020-02-11 12:27:40 +0100

    testnode + updated tf position of sensor

---
+++ graphslam_test/src/graphslam_test_node.cpp
@@ -0,0 +1,15 @@
+#include "ros/ros.h"
+
+int main(int argc, char* argv[])
+{
+	ros::init(argc, argv, "GraphSlam_Test");
+
+	ros::NodeHandle node_handle;
+
+
+	while(ros::ok())
+	{
+
+	}
+	return 0;
+}

a4019e972019d755b1be1d9026e89933cd692faa
Humam <amouri.humam85@gmail.com>
2020-02-21 12:57:00 +0100

    created a localization node

---
+++ localization/src/localization_node.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <cmath>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+
+
+#define LASERSCAN_TOPIC "scan"
+#define ODOM_TOPIC "odom"
+
+
+void laserscan_callback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  ROS_INFO("glhjjlhbj\n");
+
+  tf::TransformListener odom_listener;
+  tf::StampedTransform transform;
+  try{
+        odom_listener.lookupTransform("/odom1", "/odom2",
+                                ros::Time(0), transform);
+  }
+  catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+      ros::Duration(1.0).sleep();
+  }
+
+  // ROS_INFO("Seq: [%d]", msg->header.seq);
+  ROS_INFO("Position-> y: [%f]", transform.getOrigin().y());
+  ROS_INFO("AAAAAA\n");
+
+  // ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+}
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "localization");
+  ros::NodeHandle nh;
+  ROS_INFO("SFHAKHs\n");
+
+
+  while (ros::ok())
+  {
+	ros::Subscriber laserscan_subscriber = nh.subscribe(LASERSCAN_TOPIC, 1, laserscan_callback);
+    ros::spinOnce();
+  }
+
+
+  return 0;
+}
